3e14483f1fb2fec3a4211d9945db8caedcda1a0d
William Pollet <william.pollet@kisskissbankbank.com>
2018-12-04 21:27:21 +0100

    propper config loading

--- lib/safe_pusher/configuration.rb
+++ lib/safe_pusher/configuration.rb
@@ -8,7 +8,9 @@ module SafePusher
                   :repo_url
 
     def initialize
-      application_config = YAML.load('safe_pusher.yml')
+      application_config = YAML.load_file(
+        File.join(__dir__, './safe_pusher.yml'),
+      )
 
       @files_to_skip = application_config['files_to_skip'] || []
       @app_base_directory = application_config['app_base_directory']

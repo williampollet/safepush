aa79b521097c01bf0a33df98b99de15ae8e988df
William Pollet <polletwilliam@gmail.com>
2018-12-04 20:58:37 +0100

    Feature/adding travis (#8)

--- spec/safe_pusher_spec.rb
+++ spec/safe_pusher_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe SafePusher do
-  it "has a version number" do
+  it 'has a version number' do
     expect(SafePusher::VERSION).not_to be nil
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,18 @@
-require "bundler/setup"
-require "safe_pusher"
+require 'bundler/setup'
+require 'safe_pusher'
 require 'pry'
 
 RSpec.configure do |config|
   config.before(:all) do
-    SafePusher.configure do |config|
-      config.app_base_directory = %w[lib/]
-      config.repo_url = 'https://github.com/williampollet/safe_pusher'
+    SafePusher.configure do |safe_pusher_config|
+      safe_pusher_config.app_base_directory = %w[lib/]
+      safe_pusher_config.repo_url =
+        'https://github.com/williampollet/safe_pusher'
     end
   end
 
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!

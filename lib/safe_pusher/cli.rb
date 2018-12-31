8af997903925f9f7d4d2a5ce19ecff8bf3f5f735
William <polletwilliam@gmail.com>
2018-12-31 10:57:31 +0100

    fixing shortcuts

--- lib/safe_pusher/cli.rb
+++ lib/safe_pusher/cli.rb
@@ -10,7 +10,7 @@ module SafePusher
 
       exit results unless results == 0
     end
-    map t: :test
+    map 't' => :test
 
     desc 'lint (l)', 'launch pronto with a return message'
     def lint
@@ -22,7 +22,7 @@ module SafePusher
 
       exit results unless results == 0
     end
-    map l: :lint
+    map 'l' => :lint
 
     desc 'push (p)', 'push your code on github'
     def push
@@ -34,7 +34,7 @@ module SafePusher
 
       exit results unless results == 0
     end
-    map p: :push
+    map 'p' => :push
 
     desc 'open (o)', 'open a pull request on github'
     def open
@@ -46,7 +46,7 @@ module SafePusher
 
       exit results unless results == 0
     end
-    map o: :open
+    map 'o' => :open
 
     desc 'push_and_open (po)', 'push your code on github,'\
          ' and open a Pull Request on Github if none is openned'
@@ -59,7 +59,7 @@ module SafePusher
 
       exit results unless results == 0
     end
-    map po: :push_and_open
+    map 'po' => :push_and_open
 
     desc 'test_and_lint (tl)',
          'launch the test suite, then pronto if it is successful'
@@ -67,7 +67,7 @@ module SafePusher
       invoke :test
       invoke :lint
     end
-    map tl: :test_and_lint
+    map 'tl' => :test_and_lint
 
     desc 'lint_push_and_open (lpo)',
          'run your favorite linter, then push on github and open a'\
@@ -76,7 +76,7 @@ module SafePusher
       invoke :lint
       invoke :push_and_open
     end
-    map lpo: :lint_push_and_open
+    map 'lpo' => :lint_push_and_open
 
     desc 'test_lint_push_and_open (tlpo)',
          'run your favorite linters and tests, then push on github and open'\
@@ -86,6 +86,6 @@ module SafePusher
       invoke :lint
       invoke :push_and_open
     end
-    map tlpo: :test_lint_push_and_open
+    map 'tlpo' => :test_lint_push_and_open
   end
 end

420e7902a940dfdb2e7036adeabdd7e7fb9db57a
William <polletwilliam@gmail.com>
2019-01-05 16:43:14 +0100

    adding shortcuts

--- lib/safe_pusher/cli.rb
+++ lib/safe_pusher/cli.rb
@@ -73,7 +73,7 @@ module SafePusher
     end
 
     def arguments_valid?
-      arguments.join(' ') =~ /^(?!\s*$)(?:test|lint|push|open| )+$/
+      arguments.join(' ') =~ /^(?!\s*$)(?:test|lint|push|open|t|l|p|o| )+$/
     end
 
     def help

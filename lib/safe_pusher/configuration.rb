3332fc8e5fc9d4f74452c10e9c0470a85748e94c
William <polletwilliam@gmail.com>
2019-12-01 22:46:00 +0100

    removing useless require

--- lib/safe_pusher/configuration.rb
+++ lib/safe_pusher/configuration.rb
@@ -1,8 +1,5 @@
-require 'yaml'
-# Configuration variables and defaults
 module SafePusher
   class Configuration
-    # The configuration singleton
     attr_accessor :files_to_skip,
                   :app_base_directory,
                   :repo_url,
